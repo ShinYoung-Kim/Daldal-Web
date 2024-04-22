aaf3c54e799b1bd791e07bd14f952cfeae39ca5b
ShinYoung-Kim <lukey0515@naver.com>
2024-04-22 15:48:05 +0900

    feat: right header rendering 로직 변경

--- src/components/molecules/headers/rightHeaders/RightPointHeader.tsx
+++ src/components/molecules/headers/rightHeaders/RightPointHeader.tsx
@@ -1,18 +1,19 @@
+import { RightHeaderType } from '@type/navigation';
+
 type RightPointHeaderProps = {
-	pointType: 'imageUpload' | 'withdrawal';
+	pointType: Omit<RightHeaderType, 'default'>;
 };
 
 const RightPointHeader = ({ pointType }: RightPointHeaderProps) => {
-	const renderText = () => {
-		if (pointType === 'imageUpload') {
-			return '구매 인증 포인트 내역';
-		} else {
-			return '포인트 출금 내역';
-		}
+	const headerTitleMatching = {
+		imageUpload: '구매 인증 포인트 내역',
+		withdrawalHistory: '포인트 출금 내역',
+		withdrawal: '포인트 출금하기',
 	};
+
 	return (
 		<div className="absolute top-0 left-0 w-full py-4 flex justify-center typography-Body1 typography-M text-White">
-			{renderText()}
+			{headerTitleMatching[pointType as keyof typeof headerTitleMatching]}
 		</div>
 	);
 };
---
+++ src/type/navigation.ts
@@ -0,0 +1,6 @@
+export type LeftHeaderType = 'logo' | 'back' | 'home';
+export type RightHeaderType =
+	| 'default'
+	| 'withdrawal'
+	| 'imageUpload'
+	| 'withdrawalHistory';
